6226612b805d09d98102349a0a42da95f2d452e5
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-06-20 17:38:39 +0545

    Locomotive CMS using gem example.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Education::Application.config.secret_token = '3d90a36e255cb41e0d9871e9a6bbe5401b87f45a47941cf7aacbc0b2620b796a359c4d659767caf4dfdd529572ac64b0f7a488b3e202213e4ccdeed61d68d99b'
